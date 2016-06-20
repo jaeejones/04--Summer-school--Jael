955bf464774c05a17b2a4af4894a6ebbe9b07282
Jael <jaele.jones@gmail.com>
2016-06-20 14:11:51 -0400

    code for enrollment added

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -11,14 +11,7 @@ namespace SummerSchool
         static string[] Students = new string[15];
 
         static void Main(string[] args)
-
         {
-            Console.WriteLine("2016 Summer Student Enrollment for Hogwarts Academy");
-            Console.WriteLine("1. Student enrollment");
-            Console.WriteLine("2. Student unenrollment");
-            Console.WriteLine("3. Roster of Enrolled students");
-            Console.WriteLine("4. Exit Menu");
-
             while (true)
             {
                 Console.WriteLine("2016 Summer Student Enrollment for Hogwarts Academy");
@@ -27,22 +20,23 @@ namespace SummerSchool
                 Console.WriteLine("3. Roster of Enrolled students");
                 Console.WriteLine("4. Exit Menu");
                 //print out the menu
-                int menuchoice = Convert.ToInt32(Console.ReadLine()); // Why is the Convert.ToInt32 used?
+                int choice = Convert.ToInt32(Console.ReadLine()); // Why is the Convert.ToInt32 used?
                 if (choice == 1)
 
                 {
                     EnrollStudent(); //initiate the method to enroll students 
                     Console.WriteLine("Enter the name of the student you wish to enroll.");
+                    Console.WriteLine("Press enter to continue.");
                 }
                 else if (choice == 2)
                 {
-                    Unenrollstudent();
+                    //Unenrollstudent();
                     //Console.WriteLine("Choose 2 to unenroll a student, then press enter.");
-                    //Console.ReadKey(); I have kept this here as a reminder of what I need to do if the person does not choose to enroll. I 
+                    //Console.WriteLine("Press enter to continue.");
                 }
                 else if (choice == 3)
                 {
-                    PrintStudent();
+                    //PrintStudent();
                 }
 
                 else if (choice == 4)
@@ -50,27 +44,29 @@ namespace SummerSchool
                     //quit
                     break;
                 }
-               else 
+                else
                 {
                     //Unknown choice!
-                    Console.WriteLine("Please enter a number 1-4");
+                    Console.WriteLine("Please choose between 1-4.");
 
                 }
-             }
+            }
+
             Console.ReadKey();
         }
 
-       
 
-        static void EnrollStudent() //Jael Here is the method that you need to create to use to enroll the students
+
+        static void EnrollStudent()
         {
             Console.WriteLine("Enter the name of student to enroll.");
-            string[] student = Console.ReadLine();
+            string student = Console.ReadLine();
+
 
             // put the students in the next free spot.
             int spot = GetNextFreeSpot();
 
-            Students[spot] = student;
+
             if (spot == -1)
             {
                 Console.WriteLine("Enrollment is Full.");
@@ -79,12 +75,31 @@ namespace SummerSchool
             }
             else
             {
-                Students[spot] = stu;
+                Students[spot] = student;
                 //method to find next available spot in students
             }
+        }
+
+        static int GetNextFreeSpot()
+
+        {
+            for (int i = 0; i < Students.Length; i++)
+            {
+                if (Students[i] == null)
+                {
+                    //we found one!
+                    return i;
+                }
+            }
+
+            // if we get through the whole 
+            return -1;
 
         }
     }
+}
+
+
 
 
 
@@ -138,7 +153,7 @@ namespace SummerSchool
     Students[1] = "Bob";*/
 
 
-*/
+
 /*static void Main(string[] args) //I am confused on how to use the string/loop method to enroll students. 
 {
    string[] enrolledstudents = new string[15];
@@ -174,14 +189,4 @@ namespace SummerSchool
 //What method would I use to save the name of the student once the student has been enrolled, have we even learned this?
 
 
-/* Notes from Today
-
-static 
-
-//Finding the next free spot
-
-for (int i = 0; 1 < Students.Lenght; i++)
-
-
-/* int spot = GetNext
-                         
+   
